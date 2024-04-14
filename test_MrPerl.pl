b73d9da97e17f232c78f6227f0a49753c9c32a86
OGalloy <onedayaslion@gmail.com>
2024-04-14 22:09:04 +0500

    add new test in test_MrPerl.pm

--- test_MrPerl.pl
+++ test_MrPerl.pl
@@ -8,14 +8,17 @@ use MrPerl;
 
 use Test::More tests => 3;
 
+my $containerName = "jovial_bassi";
 
+my $start = MrPerl::startByName("jovial_bassi");
+is($start, $containerName, "test: start docker container.");
 
-#my $start = MrPerl::startByName("jovial_bassi");
-#is($start, "jovial_bassi", "test: start docker container.");
+my $inspectName = MrPerl::inspect($containerName, "Name");
+is($inspectName, $containerName, "test 'docker inspect.");
 
-#my $stop1 = MrPerl::stopByName("jovial_bassi");
-#is($stop1, "jovial_bassi", "test: stop docker container.");
+my $stop1 = MrPerl::stopByName("jovial_bassi");
+is($stop1, $containerName, "test: stop docker container.");
 
 my $stop2 = MrPerl::stopByName("jovial_bassi");
-is($stop2, "jovial_bassi", "test: stop docker container twice.")
+is($stop2, $containerName, "test: stop docker container twice.")
 
